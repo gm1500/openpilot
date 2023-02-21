30849bd79d6ec739e8dc4facb1f3b84012cdf412
morrislee <morrismurphy@gmail.com>
2023-02-20 18:33:34 -0700

    scale ego

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -57,9 +57,9 @@ class LateralPlanner:
     if len(md.position.x) == TRAJECTORY_SIZE and len(md.orientation.x) == TRAJECTORY_SIZE:
       self.path_xyz = np.column_stack([md.position.x, md.position.y, md.position.z])
       self.t_idxs = np.array(md.position.t)
-      self.plan_yaw = np.array(md.orientation.z)
-      self.plan_yaw_rate = np.array(md.orientationRate.z)
-      self.velocity_xyz = np.column_stack([md.velocity.x, md.velocity.y, md.velocity.z])
+      self.plan_yaw = np.array(md.orientation.z) * self.v_model_scale
+      self.plan_yaw_rate = np.array(md.orientationRate.z) * self.v_model_scale
+      self.velocity_xyz = np.column_stack([md.velocity.x, md.velocity.y, md.velocity.z]) * self.v_model_scale
       car_speed = np.linalg.norm(self.velocity_xyz, axis=1)
       self.v_plan = np.clip(car_speed, MIN_SPEED, np.inf)
       self.v_ego = self.v_plan[0]
