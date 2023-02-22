fbffd08d020f0c52c48b5688a731ac6a289d33eb
morrislee <morrismurphy@gmail.com>
2023-02-22 09:27:42 -0700

    xy

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -61,11 +61,11 @@ class LateralPlanner:
     # Parse model predictions
     md = sm['modelV2']
     if len(md.position.x) == TRAJECTORY_SIZE and len(md.orientation.x) == TRAJECTORY_SIZE:
-      self.path_xyz = np.column_stack([md.position.x, np.array(md.position.y) * self.v_model_scale, md.position.z])
+      self.path_xyz = np.column_stack([np.array(md.position.x) * self.v_model_scale, np.array(md.position.y) * self.v_model_scale, md.position.z])
       self.t_idxs = np.array(md.position.t)
       self.plan_yaw = np.array(md.orientation.z) * self.v_model_scale
       self.plan_yaw_rate = np.array(md.orientationRate.z) * self.v_model_scale
-      self.velocity_xyz = np.column_stack([md.velocity.x, np.array(md.velocity.y) * self.v_model_scale, md.velocity.z])
+      self.velocity_xyz = np.column_stack([np.array(md.velocity.x) * self.v_model_scale, np.array(md.velocity.y) * self.v_model_scale, md.velocity.z])
       car_speed = np.linalg.norm(self.velocity_xyz, axis=1)
       self.v_plan = np.clip(car_speed, MIN_SPEED, np.inf)
       self.v_ego = self.v_plan[0]
