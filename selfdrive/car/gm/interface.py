e1f64e23945833c9aab7f0f1ae45a1c7f6685d13
morrislee <morrismurphy@gmail.com>
2023-03-06 18:18:44 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -307,7 +307,7 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.SILVERADO:
-      ret.minEnableSpeed = 10.
+      ret.minEnableSpeed = 2.8
       ret.minSteerSpeed = -1 * CV.MPH_TO_MS
       ret.mass = 2500. + STD_CARGO_KG
       ret.wheelbase = 3.75
