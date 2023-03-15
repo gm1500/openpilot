c04edb1b12843e4187e49f0e2c575246a2a3a9e3
morrislee <morrismurphy@gmail.com>
2023-03-15 08:25:19 -0600

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -198,8 +198,8 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.5
       tire_stiffness_factor = 1.0
       ret.stopAccel = -0.25
-      #ret.longitudinalTuning.kpV = [2.4, 2.4]
-      #ret.longitudinalTuning.kiV = [0.08]
+      ret.longitudinalTuning.kpV = [2.4, 1.2]
+      ret.longitudinalTuning.kiV = [0.08]
       ret.longitudinalActuatorDelayUpperBound = 0.67
       ret.longitudinalActuatorDelayLowerBound = 0.45
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
