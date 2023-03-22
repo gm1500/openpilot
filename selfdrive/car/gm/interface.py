1fb87aa4e3f1ebf688b554f7a70bd35fc0b97f2c
morrislee <morrismurphy@gmail.com>
2023-03-22 02:23:58 -0600

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -192,12 +192,20 @@ class CarInterface(CarInterfaceBase):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.SILVERADO:
-      ret.mass = 2200. + STD_CARGO_KG
+      ret.mass = 2500. + STD_CARGO_KG
       ret.wheelbase = 3.75
       ret.steerRatio = 16.3
       ret.centerToFront = ret.wheelbase * 0.5
       tire_stiffness_factor = 1.0
+      ret.stopAccel = -0.25
+      ret.longitudinalTuning.kpV = [2.4, 1.2]
+      ret.longitudinalTuning.kiV = [0.08]
+      ret.longitudinalActuatorDelayUpperBound = 0.67
+      ret.longitudinalActuatorDelayLowerBound = 0.45
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      ret.lateralTuning.torque.kp = 2.0
+      ret.lateralTuning.torque.kf = 1.0
+      ret.lateralTuning.torque.ki = 0.0001
 
     elif candidate == CAR.EQUINOX:
       ret.mass = 3500. * CV.LB_TO_KG + STD_CARGO_KG
