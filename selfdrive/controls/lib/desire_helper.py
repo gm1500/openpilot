264e43fbf59c6100e7b60abaf51ef9850a52c0fa
morrislee <morrismurphy@gmail.com>
2023-04-17 22:16:03 -0600

    Nudgeless

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -69,7 +69,8 @@ class DesireHelper:
 
         if not one_blinker or below_lane_change_speed:
           self.lane_change_state = LaneChangeState.off
-        elif torque_applied and not blindspot_detected:
+        #elif torque_applied and not blindspot_detected:
+        elif True and not blindspot_detected:
           self.lane_change_state = LaneChangeState.laneChangeStarting
 
       # LaneChangeState.laneChangeStarting
