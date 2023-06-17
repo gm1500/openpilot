73fee36cef4a9ba62c103fd1486d2ade86d9b2b0
morrislee <morrismurphy@gmail.com>
2023-06-16 18:26:15 -0600

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -230,7 +230,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpV = [2.4, 1.2]
       ret.longitudinalTuning.kiV = [0.16]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-      ret.lateralTuning.torque.kf = 0.6
+      ret.lateralTuning.torque.kf = 1.2
 
     elif candidate == CAR.EQUINOX:
       ret.mass = 3500. * CV.LB_TO_KG + STD_CARGO_KG
