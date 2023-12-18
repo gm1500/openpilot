3fcc8925cb90e7996a67da65a7f7fcbcc9ca9ffb
morrislee <morrismurphy@gmail.com>
2023-12-18 16:01:32 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -232,7 +232,7 @@ class CarInterface(CarInterfaceBase):
       ret.stopAccel = -0.25
       ret.minSteerSpeed = -1
       ret.longitudinalTuning.kpV = [2.4, 1.5]
-      ret.longitudinalTuning.kiV = [0.72]
+      ret.longitudinalTuning.kiV = [1.0]
       # On the Bolt, the ECM and camera independently check that you are either above 5 kph or at a stop
       # with foot on brake to allow engagement, but this platform only has that check in the camera.
       # TODO: check if this is split by EV/ICE with more platforms in the future
