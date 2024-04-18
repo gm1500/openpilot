21434fa7748289f85b9c55348dd8138fa0356758
morrislee <morrismurphy@gmail.com>
2024-04-18 08:41:45 -0600

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -191,6 +191,8 @@ class CarInterface(CarInterfaceBase):
       # TODO: check if this is split by EV/ICE with more platforms in the future
       if ret.openpilotLongitudinalControl:
         ret.minEnableSpeed = -1.
+        ret.vEgoStopping = 0.50
+        ret.vEgoStarting = 0.50
         ret.stopAccel = -0.39
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
