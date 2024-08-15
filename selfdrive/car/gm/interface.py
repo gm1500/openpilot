b8527bf83bfe0db5f31b3ad2c0d76d619f89a5fb
morrislee <morrismurphy@gmail.com>
2024-08-14 21:43:40 -0600

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -242,7 +242,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.SILVERADO:
       ret.mass = 2450.
       ret.wheelbase = 3.75
-      ret.steerRatio = 17.5
+      ret.steerRatio = 12
       ret.centerToFront = ret.wheelbase * 0.5
       ret.tireStiffnessFactor = 1.0
       # On the Bolt, the ECM and camera independently check that you are either above 5 kph or at a stop
