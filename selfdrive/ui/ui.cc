d1a4fe41e134b5ced3b5a9246237cf638da06d6a
morrislee <morrismurphy@gmail.com>
2025-01-15 21:52:55 -0700

    1.64 height

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -49,7 +49,7 @@ void update_leads(UIState *s, const cereal::RadarState::Reader &radar_state, con
     auto lead_data = (i == 0) ? radar_state.getLeadOne() : radar_state.getLeadTwo();
     if (lead_data.getStatus()) {
       float z = line.getZ()[get_path_length_idx(line, lead_data.getDRel())];
-      calib_frame_to_full_frame(s, lead_data.getDRel(), -lead_data.getYRel(), z + 1.22, &s->scene.lead_vertices[i]);
+      calib_frame_to_full_frame(s, lead_data.getDRel(), -lead_data.getYRel(), z + 1.64, &s->scene.lead_vertices[i]);
     }
   }
 }
@@ -114,7 +114,7 @@ void update_model(UIState *s,
     max_distance = std::clamp((float)(lead_d - fmin(lead_d * 0.35, 10.)), 0.0f, max_distance);
   }
   max_idx = get_path_length_idx(plan_position, max_distance);
-  update_line_data(s, plan_position, 0.9, 1.22, &scene.track_vertices, max_idx, false);
+  update_line_data(s, plan_position, 0.9, 1.64, &scene.track_vertices, max_idx, false);
 }
 
 void update_dmonitoring(UIState *s, const cereal::DriverStateV2::Reader &driverstate, float dm_fade_state, bool is_rhd) {
