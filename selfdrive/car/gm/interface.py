11c723b2361df9e9d9b2b2034f928e7ecefe68f5
morrislee <morrismurphy@gmail.com>
2025-03-23 20:11:58 -0600

    0.3 delay

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -251,6 +251,7 @@ class CarInterface(CarInterfaceBase):
       if ret.openpilotLongitudinalControl:
         ret.minEnableSpeed = -1.
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      ret.steerActuatorDelay = 0.3
 
     elif candidate == CAR.EQUINOX:
       ret.mass = 3500. * CV.LB_TO_KG
