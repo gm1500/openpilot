5548d9490f452a39ac080c695706d39b4cf715b4
morrislee <morrismurphy@gmail.com>
2025-04-12 01:24:53 -0600

    75%

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -71,11 +71,11 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
 
 def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.75 * 0.5
+    return 1.75 * 0.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45 * 0.5
+    return 1.45 * 0.75
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.25 * 0.5
+    return 1.25 * 0.75
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
