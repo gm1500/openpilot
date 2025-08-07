25f23006ed2610cbe10ee6e3aa7171e0b9d8dc91
morrislee <morrismurphy@gmail.com>
2025-08-06 22:31:22 -0600

    Nudgeless

--- openpilot/selfdrive/controls/lib/desire_helper.py
+++ openpilot/selfdrive/controls/lib/desire_helper.py
@@ -51,8 +51,7 @@ class DesireHelper:
         if not one_blinker or below_lane_change_speed:
           self.lane_change_state = LaneChangeState.off
           self.lane_change_direction = LaneChangeDirection.none
-          self.lane_change_timer = 0.0
-        elif torque_applied and not blindspot_detected:
+        elif True and not blindspot_detected:
           self.lane_change_state = LaneChangeState.laneChangeStarting
           self.lane_change_timer = 0.0
 
