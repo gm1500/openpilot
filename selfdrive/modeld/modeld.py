23f238604d10d46d57e9e02ee6e711e85dd817a4
morrislee <morrismurphy@gmail.com>
2025-12-01 21:11:03 -0700

    2.0

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -43,7 +43,7 @@ POLICY_METADATA_PATH = Path(__file__).parent / 'models/driving_policy_metadata.p
 LAT_SMOOTH_SECONDS = 0.1
 LONG_SMOOTH_SECONDS = 0.3
 MIN_LAT_CONTROL_SPEED = 0.3
-RECOVERY_POWER = 1.5 # The higher this number the more aggressively the model will recover to lanecenter, too high and it will ping-pong
+RECOVERY_POWER = 2.0 # The higher this number the more aggressively the model will recover to lanecenter, too high and it will ping-pong
 
 def get_action_from_model(model_output: dict[str, np.ndarray], prev_action: log.ModelDataV2.Action,
                           lat_action_t: float, long_action_t: float, v_ego: float) -> log.ModelDataV2.Action:
